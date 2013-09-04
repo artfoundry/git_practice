0e808ee173b93106f5173b37ff33d3b6bd97fccc
artfoundry <dave@artfoundry.com>
2013-09-03 19:04:58 -0700

    done

--- git_cycle.rb
+++ git_cycle.rb
@@ -11,32 +11,26 @@
 #Exercise 1
 
 def add(n1, n2)
-  #write a block of code to return the sum 
-  #of 2 numbers then delete these comments
+  n1 + n2
 end
 
 #Exercise 2
 
-# def subtract(n1, n2)
-#     n1  n2 #fix me!!
-#   #fix the block of code to return the difference 
-#   #of 2 numbers then delete these comments
-# end
+def subtract(n1, n2)
+    n1 - n2
+end
 
 #Exercise 3
 
-# def multiply(n1, n2)
-#   return n3 
-#   #write a block of code to return the product 
-#   #of 2 numbers then delete these comments
-# end
+def multiply(n1, n2)
+ n1 * n2
+end
 
 #Exercise 4
 
-# def divide(n1, n2)
-#   return n1/n2
-#   #make necessary corrections then delete these comments
-# end
+def divide(n1, n2)
+  n1 / n2
+end
 
 
 
